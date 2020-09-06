c285c0593a9f32d9d72c112bf3b6bc284373653a
anton <anton@blackhorseteam.com>
2020-09-06 13:15:08 +0300

    Correctly working version

--- auxiliary/maxArrayDigit.js
+++ auxiliary/maxArrayDigit.js
@@ -1,18 +1,22 @@
-const arr = [2, 4, 53, 5333, 33, 764, 3];
+const arr1 = [2, 4, 53, 5333, 33, 764, 3]; 
+const arr2 = [-3, 5];
+const arr3 = [0, 22, 17, -9]; 
+const arr4 = [-22, -100, 1]; 
+const arr5 = [-22, -100, -1]; 
 
 const maxArrayDigit = (arr) => {
-  let max = 0;
   let i = 0;
-  while (i < arr.length) {
-    max = arr[i];
-    const nextValue = arr[i + 1];
-    if (nextValue && arr[i] < nextValue) {
-      max = arr[i + 1];
-      i += 1;
-    } else {
-      return max;
+  let max = arr[i];
+  for (; i < arr.length;  i += 1) {
+    if (arr[i] > max) {
+      max = arr[i];
     }
   }
+  return max
 };
 
-console.log(maxArrayDigit(arr));
+console.log(maxArrayDigit(arr1));//5333
+console.log(maxArrayDigit(arr2));// 5
+console.log(maxArrayDigit(arr3));//22
+console.log(maxArrayDigit(arr4));//1
+console.log(maxArrayDigit(arr5));//-1
