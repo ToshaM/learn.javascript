2d7c9a8eb49c51e078c7e0aefa7dbb3dc5491fe1
anton <anton@blackhorseteam.com>
2020-09-06 12:55:17 +0300

    Correctly working version

--- auxiliary/kebabToSnake.js
+++ auxiliary/kebabToSnake.js
@@ -8,9 +8,10 @@ const kebabToCamel = (str) => {
   let flag = false;
 
   for (; i < str.length; i += 1) {
-    if (str[i] === "-") {
-      // camelStr += str[i + 1].toLocaleUpperCase();
+    if (str[i] === "-" && i !== 0) {
       flag = true;
+    } else if (str[i] === "-" && i == 0) {
+      flag = false;
     } else if (flag === true) {
       camelStr += str[i].toLocaleUpperCase();
       flag = false;
@@ -23,10 +24,10 @@ const kebabToCamel = (str) => {
 };
 
 console.log(kebabToCamel(str));
-const test1 = kebabToCamel("hello-there");
+const test1 = kebabToCamel("hello-there-people");
 const test2 = kebabToCamel("-haha-hoho");
 const test3 = kebabToCamel("-magic-is-not-allowed-");
 
-console.log("hello-there-people becomes... :", test1); 
+console.log("hello-there-people becomes... :", test1);
 console.log("-haha-hoho becomes... :", test2);
 console.log("-magic-is-not-allowed- becomes... :", test3);
